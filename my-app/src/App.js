eec04a15c8f305660cbba6972ea058710d8acc0c
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-28 09:34:14 +0000

    updated general greetings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import GeneralGreeting from './components/GeneralGreetings';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
+      <GeneralGreeting greeting="My beautiful girlfriend jo"/>
     </div>
   );
 }
