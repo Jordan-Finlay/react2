361725ea109c689da28bf82a33ac1219a22753b9
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-28 09:04:50 +0000

    updated sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import css from './css/Sidebar.module.css';
+
+function Sidebar(){
+    return  <div className = {css.sidebar}><a>Link 1</a><a>Link 2</a><a>Link 3</a></div>
+}
+
+export default Sidebar
